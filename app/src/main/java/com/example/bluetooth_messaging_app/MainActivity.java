6d23364aa0dd8c969001dcbafcfc11a7373d519b
chirandevinda <e18068@eng.pdn.ac.lk>
2022-02-19 21:36:03 +0530

    add a comment by chiran

--- app/src/main/java/com/example/bluetooth_messaging_app/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hiiiiiii
+        // comment from chiran
     }
 }
