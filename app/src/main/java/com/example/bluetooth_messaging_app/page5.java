2cc809b49d683a38ed1be8ddbd15fc32b41c67de
Ruchira Tharaka <ruchirakannangara21@gmail.com>
2022-05-16 15:59:00 +0530

    Updated pages 5,6,10,11 and Created paged 7,8

--- app/src/main/java/com/example/bluetooth_messaging_app/page5.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/page5.java
@@ -1,14 +1,29 @@
 package com.example.bluetooth_messaging_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class page5 extends AppCompatActivity {
 
+    Button onlineusers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page5);
+
+        onlineusers = findViewById(R.id.onlineusersbutton);
+        onlineusers.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),page11.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/bluetooth_messaging_app/page6.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/page6.java
@@ -1,14 +1,39 @@
 package com.example.bluetooth_messaging_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class page6 extends AppCompatActivity {
 
+    Button contactbutton,invitebutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page6);
+
+        contactbutton = findViewById(R.id.contactname);
+        contactbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(getApplicationContext(),page7.class);
+                startActivity(i1);
+            }
+        });
+
+        invitebutton = findViewById(R.id.invitebutton);
+        invitebutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(getApplicationContext(),page8.class);
+                startActivity(i1);
+            }
+        });
+
     }
+
+
 }
