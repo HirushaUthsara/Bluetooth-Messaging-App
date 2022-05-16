9912722413f93ddb2f30f1db444f2f139620deec
chirandevinda <e18068@eng.pdn.ac.lk>
2022-05-16 13:44:01 +0530

    list paired devices

---
+++ app/src/main/java/com/example/bluetooth_messaging_app/list_paired_devices.java
@@ -0,0 +1,14 @@
+package com.example.bluetooth_messaging_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_paired_devices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_paired_devices);
+    }
+}
