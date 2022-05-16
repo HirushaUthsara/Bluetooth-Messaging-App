113557a2491cf87d124e2bff939984ba882d013e
Ruchira Tharaka <ruchirakannangara21@gmail.com>
2022-05-16 13:02:35 +0530

    Linked pages 5,6,10,11 to buttons

--- app/src/main/java/com/example/bluetooth_messaging_app/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/MainActivity.java
@@ -11,13 +11,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
-    Button button_for_page2_direct_chat;
+    Button button_for_page2_direct_chat,button2page5,button2page6,button2page10,button2page11;
     Button button_for_page3_group_chat, button_for_page2_3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hiiiiiii
+
+        button2page5 = findViewById(R.id.button2page5);
+        button2page5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(getApplicationContext(),page5.class);
+                startActivity(i1);
+            }
+        });
+        button2page6 = findViewById(R.id.button2page6);
+        button2page6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent(getApplicationContext(),page6.class);
+                startActivity(i2);
+            }
+        });
+        button2page10 = findViewById(R.id.button2page10);
+        button2page10.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i3 = new Intent(getApplicationContext(),page10.class);
+                startActivity(i3);
+            }
+        });
+        button2page11 = findViewById(R.id.button2page11);
+        button2page11.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i4 = new Intent(getApplicationContext(),page11.class);
+                startActivity(i4);
+            }
+        });
+
+
 
         // link to page 2 direct chat
         button_for_page2_direct_chat = findViewById(R.id.button_for_page2_direct_chat);
--- app/src/main/java/com/example/bluetooth_messaging_app/page5.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/page5.java
@@ -9,6 +9,6 @@ public class page5 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page4);
+        setContentView(R.layout.activity_page5);
     }
 }
