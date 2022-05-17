198310c5d9e0cc6e5ff4e5a2f88a38b5a821b0a1
HirushaUthsara <e18368@eng.pdn.ac.lk>
2022-05-17 16:45:22 +0530

    Modified Profile Page

--- app/src/main/java/com/example/bluetooth_messaging_app/page4.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/page4.java
@@ -2,13 +2,32 @@ package com.example.bluetooth_messaging_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class page4 extends AppCompatActivity {
+
+    private CircleImageView ProfileImage;
+    private static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page4);
+
+        ProfileImage = (CircleImageView) findViewById(R.id.profile_pic);
+        ProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent gallery = new Intent();
+
+
+            }
+        });
+
     }
 }
