4a65982cce3a0ae481ec00874e4b07f15fd22c05
chirandevinda <e18068@eng.pdn.ac.lk>
2022-05-17 18:41:01 +0530

    new page for sending sending msg

---
+++ app/src/main/java/com/example/bluetooth_messaging_app/send_and_rec_msg.java
@@ -0,0 +1,14 @@
+package com.example.bluetooth_messaging_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class send_and_rec_msg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_and_rec_msg);
+    }
+}
