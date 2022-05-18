840eaf7d10f762a3131237df0cd5d00b6ca281dc
HirushaUthsara <e18368@eng.pdn.ac.lk>
2022-05-18 11:40:15 +0530

    added methods to initialize user details

--- app/src/main/java/com/example/bluetooth_messaging_app/DBHelper.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/DBHelper.java
@@ -4,6 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+
+import androidx.annotation.Nullable;
+
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class DBHelper extends SQLiteOpenHelper {
@@ -16,6 +23,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         // tables of database constructed here
+        // useId and contactID will be basically their Mac Address
         db.execSQL("create Table UserDetails (UserID INTEGER primary key, username TEXT,user_profile_pic BLOB)");
         db.execSQL("create Table DirectContacts (ContactID INTEGER primary key, username TEXT,contact_profile_pic BLOB)");
         db.execSQL("create Table Messages (MESSAGEID INTEGER primary key AUTOINCREMENT, TIME INTEGER,SENDERID INTEGER, RECEIVERID INTEGER, CONTENT TEXT)");
@@ -49,4 +57,53 @@ public class DBHelper extends SQLiteOpenHelper {
         // close database
         sqLiteDatabase.close();
     }
+
+    public void initializeUser(String username, @Nullable Bitmap profile_pic){     // initialize user details
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("UserID",getMacAddress());
+        contentValues.put("username",username);
+
+
+        contentValues.put("user_profile_pic", (byte[]) null);
+
+        // save to table
+        sqLiteDatabase.insert("UserDetails",null,contentValues);
+        // close database
+        sqLiteDatabase.close();
+    }
+
+    public static String getMacAddress() {
+        try {
+            ArrayList<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    String hex = Integer.toHexString(b & 0xFF);
+                    if (hex.length() == 1)
+                        hex = "0".concat(hex);
+                    res1.append(hex.concat(":"));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+        }
+        return "";
+    }
+
+
 }
