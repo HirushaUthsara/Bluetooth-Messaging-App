d748eda1b24e503a66ac06e998a9598993efc2bd
Ruchira Tharaka <ruchirakannangara21@gmail.com>
2022-05-19 10:27:28 +0530

    Latest 4.0

--- app/src/main/java/com/example/bluetooth_messaging_app/Fragments/GroupsFragment.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/Fragments/GroupsFragment.java
@@ -39,6 +39,13 @@ public class GroupsFragment extends Fragment {
         Users u6 = new Users("A1",5,"Matara",",ujm yh");
         Users u7 = new Users("A1",1,"Gampaha","ehth");
         Users u8 = new Users("A1",2,"Cricket","nerts");
+        Users u9 = new Users("A1",1,"Colombo","Hi");
+        Users u10 = new Users("A1",2,"Temple","Hello");
+        Users u11 = new Users("A1",3,"Gang","wrvw");
+        Users u12 = new Users("A1",4,"Basketball","brb");
+        Users u13 = new Users("A1",5,"Matara",",ujm yh");
+        Users u14 = new Users("A1",1,"Gampaha","ehth");
+        Users u15 = new Users("A1",2,"Cricket","nerts");
 
         list.add(u1);
         list.add(u2);
@@ -47,6 +54,13 @@ public class GroupsFragment extends Fragment {
         list.add(u6);
         list.add(u7);
         list.add(u8);
+        list.add(u9);
+        list.add(u10);
+        list.add(u11);
+        list.add(u12);
+        list.add(u13);
+        list.add(u14);
+        list.add(u15);
 
 
         UserAdapter adapter = new UserAdapter(1,list,getContext());
