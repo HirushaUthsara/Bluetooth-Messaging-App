02441c34a95ec9419e9f962740e004cc987b6295
Ruchira Tharaka <ruchirakannangara21@gmail.com>
2022-05-19 10:52:24 +0530

    Done the user adapter

--- app/src/main/java/com/example/bluetooth_messaging_app/Adapters/UserAdapter.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/Adapters/UserAdapter.java
@@ -20,20 +20,21 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+//This holds the chat rows of the two main layouts
 
     ArrayList<Users> list;
     Context context;
     int DorC;
 
-    public UserAdapter(int DorC,ArrayList<Users> list, Context context) {
-        this.DorC = DorC;
+    public UserAdapter(int DorC,ArrayList<Users> list, Context context) {   //Constructor
+        this.DorC = DorC;   //This is used for identify the whether this is direct chat or a group
         this.list = list;
         this.context = context;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {     //Creating the row in the main layout
         View view = LayoutInflater.from(context).inflate(R.layout.activity_chatrow, parent,false);
 
         return new ViewHolder(view);
@@ -41,11 +42,9 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Users users = list.get(position);
-        //Picasso.get().load(users.getProfilepicture()).placeholder(R.drawable.face).into(holder.image);
-        //holder.image.setImageDrawable();
+        Users users = list.get(position);   //Getting the position of the chat in the main chat list
         if(DorC==0) {
-            switch (users.getProfilepicture()) {
+            switch (users.getProfilepicture()) {    //Selecting the profile picture of the direct chat
                 case 1:
                     holder.image.setImageResource(R.drawable.pic1);
                     break;
@@ -68,7 +67,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
                     holder.image.setImageResource(R.drawable.pic0);
                     break;
             }
-        }else{
+        }else{      //Selecting the profile picture of the group chat
             switch (users.getProfilepicture()){
                 case 1:
                     holder.image.setImageResource(R.drawable.university);
@@ -90,19 +89,19 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
                     break;
             }
         }
-        holder.username.setText(users.getUsername());
-        holder.lastmessage.setText(users.getLastmessage());
+        holder.username.setText(users.getUsername());   //set the name of the chat
+        holder.lastmessage.setText(users.getLastmessage()); //Set the last message of the chat
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        holder.itemView.setOnClickListener(new View.OnClickListener() { //When chat is clicked
             @Override
             public void onClick(View view) {
-                if(DorC==0) {
+                if(DorC==0) {   //Sending data to the chat activity of the direct chat
                     Intent i = new Intent(context, page6.class);
                     i.putExtra("userID", users.getUserID());
                     i.putExtra("username", users.getUsername());
                     i.putExtra("profilepic", users.getProfilepicture());
                     context.startActivity(i);
-                }else if(DorC==1){
+                }else if(DorC==1){  //Sending data to the chat activity of the group chat
                     Intent i = new Intent(context, page5.class);
                     i.putExtra("userID", users.getUserID());
                     i.putExtra("username", users.getUsername());
@@ -119,7 +118,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-
+    //This class holds the layout of the chat row
         ImageView image;
         TextView username, lastmessage;
         public ViewHolder(@NonNull View itemView){
