732bccc0ced69030d79a5f25639bd68fca393e1d
HirushaUthsara <e18368@eng.pdn.ac.lk>
2022-05-19 19:42:32 +0530

    Completed UI and Database

--- app/src/main/java/com/example/bluetooth_messaging_app/Fragments/DirectChatsFragment.java
+++ app/src/main/java/com/example/bluetooth_messaging_app/Fragments/DirectChatsFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bluetooth_messaging_app.Adapters.UserAdapter;
+import com.example.bluetooth_messaging_app.DBHelper;
 import com.example.bluetooth_messaging_app.Models.Users;
 import com.example.bluetooth_messaging_app.R;
 import com.example.bluetooth_messaging_app.databinding.FragmentDirectChatsBinding;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 
 
 public class DirectChatsFragment extends Fragment {
+
 
 
     public DirectChatsFragment() {
@@ -32,37 +34,10 @@ public class DirectChatsFragment extends Fragment {
         // Inflate the layout for this fragment
         binding= FragmentDirectChatsBinding.inflate(inflater, container, false);
 
+        DBHelper db = new DBHelper(this.getContext());
+        list = db.loadAllUsers();
         //I should take the arraylist from the database using a methaod and assign it to the list
         //Instead of that i created some sample data here
-        Users u1 = new Users("A1",1,"Chiran","Hi");
-        Users u2 = new Users("A2",2,"Hirusha","Hello");
-        Users u3 = new Users("A3",3,"Kalum","wrvw");
-        Users u4 = new Users("A4",4,"Wimal","brb");
-        Users u6 = new Users("A5",5,"Sahan",",ujm yh");
-        Users u7 = new Users("A6",6,"Charith","ehth");
-        Users u8 = new Users("A7",3,"Malith","nerterre");
-        Users u9 = new Users("A1",1,"Chiran","Hi !!!!");
-        Users u10 = new Users("A2",2,"Hirusha","Hello world");
-        Users u11 = new Users("A3",3,"Kalum","wssup");
-        Users u12 = new Users("A4",4,"Wimal","how are u");
-        Users u13 = new Users("A5",5,"Sahan",",fine");
-        Users u14 = new Users("A6",6,"Charith","okay then");
-        Users u15 = new Users("A7",3,"Malith","ghghjef");
-
-        list.add(u1);
-        list.add(u2);
-        list.add(u3);
-        list.add(u4);
-        list.add(u6);
-        list.add(u7);
-        list.add(u8);
-        list.add(u9);
-        list.add(u10);
-        list.add(u11);
-        list.add(u12);
-        list.add(u13);
-        list.add(u14);
-        list.add(u15);
 
         //Sending data to the corresponding chat rows
         UserAdapter adapter = new UserAdapter(0,list,getContext());
